f7f7e1e829455d0291a5350f1734b29c7874568d
Guillaume Jacquart <guillaume.jacquart@live.fr>
2022-12-15 12:11:24 +0100

    Feature/improve datacollect hit processing (#26)
    * Implement custom ticker to send batch hit regularly as well as when batch is full
    
    * Add some chatGPT generated comments
    
    Co-authored-by: Guillaume <guillaume.jacquart@abtasty.com>

--- pkg/connectors/hits_processors/datacollect.go
+++ pkg/connectors/hits_processors/datacollect.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime"
+	"sync"
 	"time"
 
 	"github.com/flagship-io/decision-api/pkg/connectors"
@@ -14,10 +15,19 @@ import (
 	"github.com/flagship-io/decision-api/pkg/utils/logger"
 )
 
+// defaultBatchingWindow is the default time duration for batching hits.
 const defaultBatchingWindow = time.Second * 30
+
+// defaultBatchSize is the default number of hits to include in a batch.
 const defaultBatchSize = 50
+
+// defaultTrackingURL is the default URL to send batched hits to.
 const defaultTrackingURL = "https://ariane.abtasty.com"
+
+// defaultLogLevel is the default log level for the DataCollect Processor.
 const defaultLogLevel = "error"
+
+// logName is the name of the logger used by the DataCollect Processor.
 const logName = "DataCollect Processor"
 
 type batchHit struct {
@@ -32,13 +42,16 @@ type DataCollectProcessor struct {
 	batchSize      int
 	trackingURL    string
 	hits           []models.MappableHit
-	ticker         *time.Ticker
+	ticker         chan time.Time
+	lastTick       time.Time
 	logger         *logger.Logger
 	httpClient     *http.Client
+	lock           *sync.Mutex
 }
 
 type DatacollectOptionBuilder func(*DataCollectProcessor)
 
+// WithBatchOptions is an option function that sets the batch size and window for the DataCollectProcessor.
 func WithBatchOptions(batchSize int, batchingWindow time.Duration) DatacollectOptionBuilder {
 	return func(l *DataCollectProcessor) {
 		l.batchSize = batchSize
@@ -46,24 +59,28 @@ func WithBatchOptions(batchSize int, batchingWindow time.Duration) DatacollectOp
 	}
 }
 
+// WithTrackingURL is an option function that sets the tracking URL for the DataCollectProcessor.
 func WithTrackingURL(url string) DatacollectOptionBuilder {
 	return func(l *DataCollectProcessor) {
 		l.trackingURL = url
 	}
 }
 
+// WithLogger is an option function that sets the logger for the DataCollectProcessor.
 func WithLogger(lvl string, fmt logger.LogFormat) DatacollectOptionBuilder {
 	return func(l *DataCollectProcessor) {
 		l.logger = logger.New(lvl, fmt, logName)
 	}
 }
 
+// WithHTTPClient is an option function that sets the HTTP client for the DataCollectProcessor.
 func WithHTTPClient(client *http.Client) DatacollectOptionBuilder {
 	return func(l *DataCollectProcessor) {
 		l.httpClient = client
 	}
 }
 
+// NewDataCollectProcessor creates a new DataCollectProcessor with the given options.
 func NewDataCollectProcessor(opts ...DatacollectOptionBuilder) *DataCollectProcessor {
 	processor := &DataCollectProcessor{
 		batchingWindow: defaultBatchingWindow,
@@ -74,6 +91,7 @@ func NewDataCollectProcessor(opts ...DatacollectOptionBuilder) *DataCollectProce
 		httpClient: &http.Client{
 			Timeout: 2 * time.Second,
 		},
+		lock: &sync.Mutex{},
 	}
 
 	for _, o := range opts {
@@ -81,28 +99,41 @@ func NewDataCollectProcessor(opts ...DatacollectOptionBuilder) *DataCollectProce
 	}
 
 	processor.logger.Info("initializing datacollect hits processor")
-	processor.ticker = time.NewTicker(processor.batchingWindow)
+	processor.ticker = make(chan time.Time)
 
 	go func() {
-		for range processor.ticker.C {
-			err := processor.sendBatchHit(context.Background())
-			if err != nil {
-				processor.logger.Errorf("error when sending batch hit: %v", err)
+		for {
+			time.Sleep(processor.batchingWindow)
+			processor.lock.Lock()
+			durationSinceLastTick := time.Since(processor.lastTick)
+			// If last tick was trigger in between because of full batch, wait a little more
+			if durationSinceLastTick < processor.batchingWindow {
+				time.Sleep(processor.batchingWindow - durationSinceLastTick)
 			}
+			processor.lock.Unlock()
+			processor.ticker <- time.Now()
+		}
+	}()
+
+	go func() {
+		for t := range processor.ticker {
+			processor.sendHits(processor.hits, t)
+			processor.hits = []models.MappableHit{}
 		}
 	}()
 
 	return processor
 }
 
-func (d *DataCollectProcessor) sendBatchHit(ctx context.Context) error {
-	if len(d.hits) == 0 {
+// sendBatchHit sends a batch of hits to the trackingURL using the httpClient.
+func (d *DataCollectProcessor) sendBatchHit(ctx context.Context, mappableHits []models.MappableHit) error {
+	if len(mappableHits) == 0 {
 		d.logger.Info("no hits to send")
 		return nil
 	}
 
 	hits := []map[string]interface{}{}
-	for _, h := range d.hits {
+	for _, h := range mappableHits {
 		h.ComputeQueueTime()
 		hits = append(hits, h.ToMap())
 	}
@@ -123,7 +154,7 @@ func (d *DataCollectProcessor) sendBatchHit(ctx context.Context) error {
 		return fmt.Errorf("error when marshaling batch hit: %v", err)
 	}
 
-	d.logger.Infof("sending hits to datacollect: %v", string(json_data))
+	d.logger.Infof("sending %d hits to datacollect: %v", len(batchHit.Hits), string(json_data))
 	req, err := http.NewRequest(http.MethodPost, d.trackingURL, bytes.NewBuffer(json_data))
 	req = req.WithContext(ctx)
 	if err != nil {
@@ -141,10 +172,21 @@ func (d *DataCollectProcessor) sendBatchHit(ctx context.Context) error {
 	}
 	d.logger.Infof("%d hits sent to datacollect successfully", len(hits))
 
-	d.hits = []models.MappableHit{}
 	return nil
 }
 
+func (d *DataCollectProcessor) sendHits(hits []models.MappableHit, tick time.Time) {
+	err := d.sendBatchHit(context.Background(), hits)
+	if err != nil {
+		d.logger.Errorf("error when sending batch hit: %v", err)
+	}
+	d.lock.Lock()
+	d.lastTick = tick
+	d.lock.Unlock()
+}
+
+// TrackHits adds the given hits to the processor for tracking.
+// If the number of hits in the processor exceeds the batch size, a batch of hits is sent.
 func (d *DataCollectProcessor) TrackHits(hits connectors.TrackingHits) error {
 	mappableHits := []models.MappableHit{}
 	for _, ca := range hits.CampaignActivations {
@@ -155,15 +197,14 @@ func (d *DataCollectProcessor) TrackHits(hits connectors.TrackingHits) error {
 	}
 	d.hits = append(d.hits, mappableHits...)
 	if len(d.hits) >= d.batchSize {
-		d.ticker.Reset(d.batchingWindow)
-		err := d.sendBatchHit(context.Background())
-		if err != nil {
-			d.logger.Errorf("error when sending batch hit: %v", err)
-		}
+		go d.sendHits(d.hits, time.Now())
+		d.lock.Lock()
+		d.hits = []models.MappableHit{}
+		d.lock.Unlock()
 	}
 	return nil
 }
 
 func (d *DataCollectProcessor) Shutdown(ctx context.Context) error {
-	return d.sendBatchHit(ctx)
+	return d.sendBatchHit(ctx, d.hits)
 }
